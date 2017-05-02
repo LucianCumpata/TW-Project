345314c60286f7bfbc1f78696298d135d60f3f86
roscaandreea <andreea.rosca@info.uaic.ro>
2017-05-02 22:10:48 +0300

    added new server, updated simple server

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var http = require("http");
+var fs = require('fs');
+var parser = require('url');
 http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write("<h1>Hello, World!</h1>");
-  res.end();
-  }).listen(4321);
-console.log("Now listening on port 4321");
+ // res.writeHead(200, {"Content-Type": "text/html"});
+ // res.write("<h1>Hello, World!</h1>");
+ // res.end();
+
+try{
+    var data = fs.readFileSync("public/index.html");
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(data);
+    res.end();
+	} catch (e) {
+	res.writeHead(404, {'Content-Type': 'text/plain'}); //text/plain
+	res.write("No route registered for " + url.pathname);
+ 	res.end();
+}
+}).listen(4321);
+  console.log("Now listening on port 4321");
---
+++ server_app.js
@@ -0,0 +1,21 @@
+// pure node.js route handling
+//https://gist.github.com/jeffrafter/353700
+var sys = require('sys');
+var http = require('http');
+var router = require('./router');
+// Handle your routes here, put static pages in ./public and they will server
+router.register('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World');
+  res.end();
+});
+
+// We need a server which relies on our router
+var server = http.createServer(function (req, res) {
+  handler = router.route(req);
+    handler.process(req, res);
+});
+
+// Start it up
+server.listen(8000);
+console.log('Server running');
