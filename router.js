ebc5a65ecb2a85e889e381c616250c965fa803c8
roscaandreea <andreea.rosca@info.uaic.ro>
2017-05-07 19:54:41 +0300

    added mime type handling, small update to server

--- router.js
+++ router.js
@@ -3,6 +3,25 @@ var fs = require('fs');
 var parser = require('url');
 var handlers = {};
 
+
+function mimeType(path){
+  //mime types from: https://gist.github.com/lsauer/5196979
+  var data = fs.readFileSync('server/mime.json')
+  var mimeObj = JSON.parse( data );
+
+  var pathExt = path.substr( path.indexOf('.') + 1 );
+  //console.log('pathExt: ', pathExt);
+
+  var mimeType;
+  Object.keys(mimeObj).forEach(function(key) {
+    if (key == pathExt) {
+      //console.log('mimeObj[key]: ', mimeObj[key]);
+      mimeType = mimeObj[key];
+    }
+  });
+  return mimeType || 'text/html';
+}
+
 exports.clear = function() {
   handlers = {};
 }
@@ -20,20 +39,26 @@ exports.route = function(req) {
 
 exports.missing = function(req) {
   // Try to read the file locally, this is a security hole, yo /../../etc/passwd
- var url = parser.parse(req.url, true);
+  var url = parser.parse(req.url, true);
   var path = __dirname + "/public" + url.pathname
+
+  console.log(req.method, url.pathname);
+
   try {    
     data = fs.readFileSync(path);
-   mime = req.headers.accepts || 'text/html'
-   return handlerFactory.createHandler(function(req, res) {
-     res.writeHead(200, {'Content-Type': mime});
-     res.write(data);
+    //mime = req.headers.accepts || 'text/html'
+    mime = mimeType(path);
+    console.log('  mime selected: ', mime);
+
+    return handlerFactory.createHandler(function(req, res) {
+      res.writeHead(200, {'Content-Type': mime});
+      res.write(data);
       res.end();
     });        
   } catch (e) { 
     return handlerFactory.createHandler(function(req, res) {
       res.writeHead(404, {'Content-Type': 'text/plain'});
-     res.write("No route registered for " + url.pathname);
+      res.write("No route registered for " + url.pathname);
       res.end();
     });      
   }  
--- server_app.js
+++ server_app.js
@@ -1,36 +1,43 @@
 // pure node.js route handling
 //https://gist.github.com/jeffrafter/353700
-//var sys = require('sys');
+
 var http = require('http');
 var router = require('./router');
-var utils=require('./server/utils');
-var mongoose=require('mongoose');
-var dbConfig=require('./server/db');
+var utils = require('./server/utils');
+
+var mongoose = require('mongoose');
+var dbConfig = require('./server/db');
 // sys deprecated, use console.log instead
+
 //DB connection
 db = mongoose.connect(dbConfig.url, function (err) {
- if (err) {
+  if (err) {
     console.log(err);
   }
   console.log('Connected to DB.');
 });
 
+
 // Handle your routes here, put static pages in ./public and they will server
 router.register('/', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  //res.write('Hello World');
-  utils.redirect(req, res, 'index.html')
+  console.log(req.method, '/');
 
-  //res.close deprecated
- res.end();
+  if(req.method == 'GET'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.write('Hello World');
+    utils.redirect(req, res, 'index.html')
+
+    //res.close deprecated
+    res.end();
+  }
 });
 
 // We need a server which relies on our router
 var server = http.createServer(function (req, res) {
   handler = router.route(req);
-    handler.process(req, res);
+  handler.process(req, res);
 });
 
 // Start it up
 server.listen(4322);
-console.log('Server running'); 
+console.log('Server running');
