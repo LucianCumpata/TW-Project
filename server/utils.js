630c6a5095d232db91bfea63d73cbeb8e148027b
roscaandreea <andreea.rosca@info.uaic.ro>
2017-06-16 18:33:13 +0300

    small backend fixes

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,10 @@
 //Utility functions
 
 exports.redirect = function (req, res, address){
+  // HACK: if using this on 404 createHandler call, it will add indefinetly to the path ...
+  // req.url += address;
+
+  req.url = '/';
   req.url += address;
 
   res.writeHead(302, {'Location': req.url});
