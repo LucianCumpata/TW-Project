eaa945058171634dc3176d8ce3ed82fcf966a950
roscaandreea <andreea.rosca@info.uaic.ro>
2017-06-16 17:16:47 +0300

    updated register and login forms, added API routes for these

--- server_app.js
+++ server_app.js
@@ -9,6 +9,9 @@ var mongoose = require('mongoose');
 var dbConfig = require('./server/db');
 // sys deprecated, use console.log instead
 
+var requestBody;
+var requestParams;
+
 //DB connection
 db = mongoose.connect(dbConfig.url, function (err) {
   if (err) {
@@ -32,10 +35,84 @@ router.register('/', function(req, res) {
   }
 });
 
+// *************** API ***********************************
+router.register('/api/register', function(req, res) {
+  console.log(req.method, '/api/register');
+
+  if(req.method == 'GET'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(
+      JSON.stringify({
+        "name": "AnoFeT",
+        "route": "/api/register"
+      }, null, 2)
+    );
+
+    res.end();
+  } else if(req.method == 'POST') {
+    console.log(req.headers);
+    //console.log(req.body); //undefined
+    console.log(requestBody); //this should work
+
+    if (requestBody){
+      utils.redirect(req, res, 'login.html');
+    } else {
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      res.write(
+        JSON.stringify({
+          "response": "POST request body is empty.",
+          "code": 500
+        }, null, 2)
+      );
+    }
+    
+    res.end();
+  }
+});
+
+// initial API ....
+router.register('/api/login', function(req, res) {
+  console.log(req.method, '/api/login');
+
+  if(req.method == 'GET'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(
+      JSON.stringify({
+        "name": "AnoFeT",
+        "route": "/api/login"
+      }, null, 2)
+    );
+    
+    res.end();
+  }
+});
+
+// *************** END API ***********************************
+
 // We need a server which relies on our router
 var server = http.createServer(function (req, res) {
-  handler = router.route(req);
-  handler.process(req, res);
+  requestBody = [];
+  //requestParams = {};
+
+  //https://stackoverflow.com/questions/5083914/get-the-whole-response-body-when-the-response-is-chunked
+  if(req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE'){
+    req.on('data', chunk => {
+      console.log('A chunk of data has arrived: ', chunk);
+      requestBody.push(chunk);
+    });
+    req.on('end', () => {
+      console.log('No more data');
+      requestBody = Buffer.concat(requestBody).toString();
+
+      handler = router.route(req);
+      handler.process(req, res);
+    });
+  } else {
+    handler = router.route(req);
+    handler.process(req, res);
+  }
+
+  //requestParams = utils.params(req);
 });
 
 // Start it up
