5104542dd9e75b1e6a5534011c4c3dd232347a57
roscaandreea <andreea.rosca@info.uaic.ro>
2017-06-17 09:54:24 +0300

    updated register controller with save and added login checks and hashed register password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,36 +1,44 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 //email regex from: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
 
 /*user model structure*/
 var userSchema = new Schema({
-	user_name:{
-		type:String,
-		require:true
-	},
-	password:{
-		type:String,
-		require:true
-	},
-	email:{
-		type:String,
-		require:true,
-		match:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-	},
-	avatar: {
+  user_name:{
+    type:String,
+    require:true
+  },
+  password:{
+    type:String,
+    require:true
+  },
+  email:{
+    type:String,
+    require:true,
+    match:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  },
+  avatar: {
     type: String,
     default: 'http://placehold.it/350x250'
   },
   role: {
-  	type: String,
-  	default: 'interviewer'
+    type: String,
+    default: 'interviewer'
   },
   registerDate: {
     type: Date,
     default: Date.now
   }
-
-
 });
+
+userSchema.methods.hashPassword = function (pass) {
+  return bcrypt.hashSync(pass, 10);//bcrypt.genSaltSync(10), null);
+};
+
+userSchema.methods.checkPassword = function (pass) {
+  return bcrypt.compareSync(pass, this.password);
+};
+
 module.exports = mongoose.model('user', userSchema);
--- server_app.js
+++ server_app.js
@@ -2,15 +2,19 @@
 //https://gist.github.com/jeffrafter/353700
 
 var http = require('http');
+var querystring = require('querystring');
+
 var router = require('./router');
 var utils = require('./server/utils');
 
 var mongoose = require('mongoose');
 var dbConfig = require('./server/db');
-// sys deprecated, use console.log instead
+var bcrypt = require('bcrypt');
 
 var requestBody;
 var requestParams;
+
+
 
 //DB connection
 db = mongoose.connect(dbConfig.url, function (err) {
@@ -36,6 +40,9 @@ router.register('/', function(req, res) {
 });
 
 // *************** API ***********************************
+
+var User = require('./server/models/userModel');
+
 router.register('/api/register', function(req, res) {
   console.log(req.method, '/api/register');
 
@@ -50,11 +57,29 @@ router.register('/api/register', function(req, res) {
 
     res.end();
   } else if(req.method == 'POST') {
-    console.log(req.headers);
+    //console.log(req.headers);
     //console.log(req.body); //undefined
-    console.log(requestBody); //this should work
+    //console.log(requestBody); //this should work
 
     if (requestBody){
+      var requestBodyJSON = querystring.parse(requestBody);
+
+      var newUser = new User(requestBodyJSON);
+      //hash the password for safe storage
+      newUser.password = newUser.hashPassword(newUser.password); //bcrypt.hashSync(requestBodyJSON.password, 10);
+
+      newUser.save(function (err, message) {
+        if (err) {
+          console.log('mongoose: ', err);
+          return res.write(
+            JSON.stringify({
+              "response": err
+            }, null, 2)
+          );
+          //return res.end();
+        }
+      });
+
       utils.redirect(req, res, 'login.html');
     } else {
       res.writeHead(500, {'Content-Type': 'application/json'});
@@ -70,7 +95,7 @@ router.register('/api/register', function(req, res) {
   }
 });
 
-// initial API ....
+
 router.register('/api/login', function(req, res) {
   console.log(req.method, '/api/login');
 
@@ -84,6 +109,70 @@ router.register('/api/login', function(req, res) {
     );
     
     res.end();
+  } else if(req.method == 'POST') {
+    console.log(requestBody);
+
+    if (requestBody){
+      var requestBodyJSON = querystring.parse(requestBody);
+      //hash the password for safe storage
+      //requestBodyJSON.password = User.hashPassword(requestBodyJSON.password); // bcrypt.hashSync(requestBodyJSON.password, 10);
+      console.log(requestBodyJSON);
+
+      User.findOne({'user_name': requestBodyJSON.user_name}, function (err, user) {
+        if (err) {
+          console.log('mongoose: ', err);
+          res.writeHead(500, {'Content-Type': 'application/json'});
+          return res.write(
+            JSON.stringify({
+              "response": err
+            }, null, 2)
+          );
+        }
+        if (!user) {
+          console.log('mongoose: ', 'User not found !');
+          res.writeHead(404, {'Content-Type': 'application/json'});
+          return res.write(
+            JSON.stringify({
+              "response": "User not found !"
+            }, null, 2)
+          );
+          //return res.send("not found !");//done(null, false, console.log({message: 'User not found'}));
+        }
+        if (!user.checkPassword(requestBodyJSON.password)) {
+          console.log('mongoose: ', 'Wrong password !');
+          //return done(null, false, console.log({message: 'Wrong password'}));
+          res.writeHead(404, {'Content-Type': 'application/json'});
+          return res.write(
+            JSON.stringify({
+              "response": "Wrong user or password !"
+            }, null, 2)
+          );
+        }
+        console.log('User %s has logged in.', user.user_name);
+        return;
+      }).then(function(){
+        //console.log('res: ', res);//
+        //console.log(res.getHeaders());
+
+        if(res.statusCode == 404 || res.statusCode == 500){
+          res.end();
+        } else {
+          utils.redirect(req, res, 'profile.html');
+        }
+
+      });
+
+
+    } else {
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      res.write(
+        JSON.stringify({
+          "response": "POST request body is empty.",
+          "code": 500
+        }, null, 2)
+      );
+      res.end();
+    }
   }
 });
 
